bc050f9291769c3c0aa427180bbb5b972c3044d1
vitalik abrosimov <vitalikabrosimov00@gmail.com>
2026-08-26 11:55:58 +0300

    корректировка страницы user

--- app/src/pages/UserPage.vue
+++ app/src/pages/UserPage.vue
@@ -41,14 +41,14 @@ const handleLogout = async () => {
 <template>
 
   <div class="user h-100 flex-center">
-    <div class="grid-cols-2 gap-20 w-75">
+    <div class="flex gap-20">
       <div class="user__avatar img-container rounded-20">
         <img :src="`${userStore.user.avatarUrl}?t=${Date.now()}`"
              :alt="userStore.user.name"
         >
       </div>
 
-      <form novalidate class="flex flex-column gap-20">
+      <form novalidate class="user__form flex flex-column gap-20">
         <p class="h4 text-w600">Данные пользователя</p>
 
         <LabelUi for-id="name" text="Имя:">
