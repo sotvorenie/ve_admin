a1441a585e9b603a8ccd0a8d9207abec172e7e06
vitalik abrosimov <vitalikabrosimov00@gmail.com>
2026-08-30 13:38:09 +0300

    все запросы music для veMusic

--- app/src/api/veMusic/artist.ts
+++ app/src/api/veMusic/artist.ts
@@ -32,8 +32,8 @@ export const apiUploadArtistAvatar = async (id: number, file: File, signal?: Abo
     return apiPost(`/artist/upload_avatar/${id}`, formData, signal)
 }
 
-export const apiAddAvatarUrlForArtist = async (id: number, avatarUrl: string, signal?: AbortSignal): Promise<SuccessResponseType> => {
-    return apiPost(`/artist/add_avatar_url/${id}`, {url: avatarUrl}, signal)
+export const apiRedactAvatarUrlForArtist = async (id: number, avatarUrl: string, signal?: AbortSignal): Promise<SuccessResponseType> => {
+    return apiPost(`/artist/redact_avatar_url/${id}`, {url: avatarUrl}, signal)
 }
 
 export const apiDeleteArtistAvatar = async (id: number, signal?: AbortSignal): Promise<SuccessResponseType> => {
---
+++ app/src/api/veMusic/music.ts
@@ -0,0 +1,44 @@
+import {MusicListType} from "@/types/music.ts";
+import {SuccessResponseType} from "@/types/success.ts";
+
+import {apiDelete, apiGet, apiPatch, apiPost} from "@/api";
+
+export const apiGetAllMusic = async (name: string = '', genreId: number = -1, page: number = 1, limit: number = 30, signal?: AbortSignal): Promise<MusicListType> => {
+    return apiGet(`/music/list?page=${page}&limit=${limit}&name=${name}&genre_id=${genreId}`, undefined, signal)
+}
+
+export const apiRedactMusic = async (id: number, title: string, genreId: number, artists: number[], signal?: AbortSignal): Promise<SuccessResponseType> => {
+    return apiPatch(`/music/redact/${id}`, {
+        title,
+        genre_id: genreId,
+        artists,
+    }, signal)
+}
+
+export const apiRedactAudioUrlForMusic = async (id: number, audioUrl: string, signal?: AbortSignal): Promise<SuccessResponseType> => {
+    return apiPost(`/music/redact_audio_url/${id}`, {url: audioUrl}, signal)
+}
+
+export const apiRedactPreviewUrlForMusic = async (id: number, audioUrl: string, signal?: AbortSignal): Promise<SuccessResponseType> => {
+    return apiPost(`/music/redact_preview_url/${id}`, {url: audioUrl}, signal)
+}
+
+export const apiRedactVideoUrlForMusic = async (id: number, audioUrl: string, signal?: AbortSignal): Promise<SuccessResponseType> => {
+    return apiPost(`/music/redact_video_url/${id}`, {url: audioUrl}, signal)
+}
+
+export const apiRedactAuditionsForMusic = async (id: number, auditionsCount: number, signal?: AbortSignal): Promise<SuccessResponseType> => {
+    return apiPatch(`/music/redact_auditions/${id}`, {auditions_count: auditionsCount}, signal)
+}
+
+export const apiDeleteMusic = async (id: number, signal?: AbortSignal): Promise<SuccessResponseType> => {
+    return apiDelete(`/music/delete/${id}`, undefined, signal)
+}
+
+export const apiDeleteMusicPreview = async (id: number, signal?: AbortSignal): Promise<SuccessResponseType> => {
+    return apiPatch(`/music/delete_preview/${id}`, undefined, signal)
+}
+
+export const apiDeleteMusicVideo = async (id: number, signal?: AbortSignal): Promise<SuccessResponseType> => {
+    return apiPatch(`/music/delete_video/${id}`, undefined, signal)
+}
