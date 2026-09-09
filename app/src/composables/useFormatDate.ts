6fe0ce8ea819fda0f9a4dda885c8aa03d9b61ede
vitalik abrosimov <vitalikabrosimov00@gmail.com>
2026-09-09 12:50:40 +0300

    обновлен formattedDate

--- app/src/composables/useFormatDate.ts
+++ app/src/composables/useFormatDate.ts
@@ -3,7 +3,7 @@ export const formatDate = (str: string) => {
     const date = new Date(str)
     if (Number.isNaN(date.getTime())) return "Неизвестно"
     const day = String(date.getDate()).padStart(2, '0')
-    const month = String(date.getMonth()).padStart(2, '0')
-    const year = String(date.getFullYear()).padStart(2, '0')
+    const month = String(date.getMonth() + 1).padStart(2, '0')
+    const year = String(date.getFullYear())
     return `${day}-${month}-${year}`
 }
